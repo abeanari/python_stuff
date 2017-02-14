3ae2204250dbf9682001f3dfd4ae14c58baa355d
abeanari <peter4bbit@gmail.com>
2017-02-13 16:13:32 -0800

    acronyms init commit

---
+++ acronyms/acronym.py
@@ -0,0 +1,51 @@
+import re
+
+
+def acro_search():
+    acronyms = []
+    for x in re.findall(PATTERN,test_string):
+        x.strip()
+        if (len(x)>1):
+            acronyms.append(x)
+    return (acronyms)
+
+def backTrackAll(C, X, Y, i, j):
+    if i == 0 or j == 0:
+        return set([""])
+    elif X[i-1] == Y[j-1]:
+        return set([Z + X[i-1] for Z in backTrackAll(C, X, Y, i-1, j-1)])
+    else:
+        R = set()
+        if C[i][j-1] >= C[i-1][j]:
+            R.update(backTrackAll(C, X, Y, i, j-1))
+        if C[i-1][j] >= C[i][j-1]:
+            R.update(backTrackAll(C, X, Y, i-1, j))
+        return R
+
+def lcs(a, b):
+    lengths = [[0 for j in range(len(b)+1)] for i in range(len(a)+1)]
+    # row 0 and column 0 are initialized to 0 already
+    for i, x in enumerate(a):
+        for j, y in enumerate(b):
+            if x == y:
+                lengths[i+1][j+1] = lengths[i][j] + 1
+            else:
+                lengths[i+1][j+1] = max(lengths[i+1][j], lengths[i][j+1])
+    # read the substring out from the matrix
+    result = ""
+    x, y = len(a), len(b)
+    while x != 0 and y != 0:
+        if lengths[x][y] == lengths[x-1][y]:
+            x -= 1
+        elif lengths[x][y] == lengths[x][y-1]:
+            y -= 1
+        else:
+            assert a[x-1] == b[y-1]
+            result = a[x-1] + result
+            x -= 1
+            y -= 1
+    return result
+
+test_string = "DOE Department of Energy FBI CIA  Rawr rAwr RAwe RaWR RAWr rAWR sigNATURE"
+PATTERN = r'[A-Z][A-Z][A-Z]+'
+print(acro_search())
